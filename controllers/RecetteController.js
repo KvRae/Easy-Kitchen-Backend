84ef56930baea516bb6ad7a3d860d937520a80af
medyassin-khabthani <mohamedyassine.khabthani@esprit.tn>
2022-12-27 12:16:07 +0100

    blog completed

--- controllers/RecetteController.js
+++ controllers/RecetteController.js
@@ -131,7 +131,14 @@ if (!obj.usersDisliked.includes(req.body.id) ){
 // add recette
 exports.add = async (req, res) => {
 
-    const { name,description ,image,isBio,duration,person,difficulty,userId} = req.body;
+    const { name,description ,image,isBio,duration,person,difficulty,userId,
+        strIngredient1,strIngredient2,strIngredient3,strIngredient4,strIngredient5,
+        strIngredient6,strIngredient7,strIngredient8,strIngredient9,strIngredient10,
+        strIngredient11,strIngredient12,strIngredient13,strIngredient14,strIngredient15,
+        strIngredient16,strIngredient17,strIngredient18,strIngredient19,strIngredient20,
+        strMeasure1,strMeasure2,strMeasure3,strMeasure4,strMeasure5,strMeasure6,strMeasure7,
+        strMeasure8,strMeasure9,strMeasure10,strMeasure11,strMeasure12,strMeasure13,strMeasure14,
+        strMeasure15,strMeasure16,strMeasure17,strMeasure18,strMeasure19,strMeasure20} = req.body;
 
     const newRecette = new recette()
     newRecette.name = name
@@ -142,6 +149,51 @@ exports.add = async (req, res) => {
     newRecette.person = person
     newRecette.difficulty = difficulty
     newRecette.user= userId
+    newRecette.strIngredient1=strIngredient1,
+    newRecette.strIngredient2=strIngredient2,
+    newRecette.strIngredient3=strIngredient3,
+    newRecette.strIngredient4=strIngredient4,
+    newRecette.strIngredient5=strIngredient5,
+    newRecette.strIngredient6=strIngredient6,
+    newRecette.strIngredient7=strIngredient7,
+    newRecette.strIngredient8=strIngredient8,
+    newRecette.strIngredient9=strIngredient9,
+    newRecette.strIngredient10=strIngredient10,
+    newRecette.strIngredient11=strIngredient11,
+    newRecette.strIngredient12=strIngredient12,
+    newRecette.strIngredient13=strIngredient13,
+    newRecette.strIngredient14=strIngredient14,
+    newRecette.strIngredient15=strIngredient15,
+    newRecette.strIngredient16=strIngredient16,
+    newRecette.strIngredient17=strIngredient17,
+    newRecette.strIngredient18=strIngredient18,
+    newRecette.strIngredient19=strIngredient19,
+    newRecette.strIngredient20=strIngredient20,
+    newRecette.strIngredient20=strIngredient20,
+    newRecette.strMeasure1=strMeasure1,
+    newRecette.strMeasure2=strMeasure2,
+    newRecette.strMeasure3=strMeasure3,
+    newRecette.strMeasure4=strMeasure4,
+    newRecette.strMeasure5=strMeasure5,
+    newRecette.strMeasure6=strMeasure6,
+    newRecette.strMeasure7=strMeasure7,
+    newRecette.strMeasure8=strMeasure8,
+    newRecette.strMeasure9=strMeasure9,
+    newRecette.strMeasure10=strMeasure10,
+    newRecette.strMeasure11=strMeasure11,
+    newRecette.strMeasure12=strMeasure12,
+    newRecette.strMeasure13=strMeasure13,
+    newRecette.strMeasure14=strMeasure14,
+    newRecette.strMeasure15=strMeasure15,
+    newRecette.strMeasure16=strMeasure16,
+    newRecette.strMeasure17=strMeasure17,
+    newRecette.strMeasure18=strMeasure18,
+    newRecette.strMeasure19=strMeasure19,
+    newRecette.strMeasure20=strMeasure20,
+
+
+
+    
     newRecette.save();
 
     await user.updateOne({ _id: userId },
--- models/recette.js
+++ models/recette.js
@@ -12,8 +12,7 @@ const recetteSchema = new mongoose.Schema({
     },
     image: {
         type: String,
-        required: true,
-        unique:true
+        required: true
     },
     isBio: {
         type: Boolean,
@@ -44,7 +43,143 @@ const recetteSchema = new mongoose.Schema({
          ref: "Comment" }],
     user: {
          type: schema.Types.ObjectId,
-          ref: "User" }
+          ref: "User" },
+          
+          strIngredient1: {
+              type: String,
+          },
+          strIngredient2: {
+              type: String,
+          },
+          strIngredient3: {
+              type: String,
+          },
+          strIngredient4: {
+              type: String,
+          },
+          strIngredient5: {
+              type: String,
+          },
+          strIngredient6: {
+              type: String,
+          },
+          strIngredient7: {
+              type: String,
+          },
+          strIngredient8: {
+              type: String,
+          },
+          strIngredient9: {
+          type: String},
+          strIngredient10: {
+              type: String,
+          },
+          strIngredient11: {
+              type: String,
+          },
+          strIngredient12: {
+              type: String,
+          },
+          strIngredient13: {
+              type: String,
+          },
+          strIngredient14: {
+              type: String,
+          },
+          strIngredient15: {
+              type: String,
+          },
+          strIngredient16: {
+              type: String,
+          },
+          strIngredient17: {
+              type: String,
+          },
+          strIngredient18: {
+              type: String,
+          },
+          strIngredient19: {
+              type: String,
+          },
+          strIngredient20: {
+              type: String,
+          },
+          strMeasure1: {
+              type: String,
+          },
+          strMeasure2: {
+              type: String,
+          }
+          ,
+          strMeasure3: {
+              type: String,
+          }
+          ,
+          strMeasure4: {
+              type: String,
+          }
+          ,
+          strMeasure5: {
+              type: String,
+          }
+          ,
+          strMeasure6: {
+              type: String,
+          }
+          ,
+          strMeasure7: {
+              type: String,
+          }
+          ,
+          strMeasure8: {
+              type: String,
+          },
+          strMeasure9: {
+              type: String,
+          }
+          ,
+          strMeasure10: {
+              type: String,
+          }
+          ,
+          strMeasure11: {
+              type: String,
+          }
+          ,
+          strMeasure12: {
+              type: String,
+          }
+          ,
+          strMeasure13: {
+              type: String,
+          }
+          ,
+          strMeasure14: {
+              type: String,
+          },
+          strMeasure15: {
+              type: String,
+          },
+          strMeasure16: {
+              type: String,
+          }
+          ,
+          strMeasure17: {
+              type: String,
+      
+          }
+          ,
+          strMeasure18: {
+              type: String,
+          }
+          ,
+          strMeasure19: {
+              type: String,
+          }
+          ,
+          strMeasure20: {
+              type: String,
+          }
 }, {timestamps: true})
 
 module.exports = mongoose.model('Recette', recetteSchema)
